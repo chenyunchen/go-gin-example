2768c783ad7d17571a005667870c949650a473e3
Tim Ling <tim.ling@silkrode.com.tw>
2020-08-06 11:22:24 +0800

    add example

---
+++ mock_example.go
@@ -0,0 +1,26 @@
+package main
+
+func init() {
+	tplMap.Store("example", Template{
+		Resp: `{"status":"ok"}`,
+		CallbackURL: `{{ index .query.notify_url 0 }}` +
+			`?time={{ index .query.time 0 }}` +
+			`&sign={{ index .query.sign 0 }}`,
+		// header 首字 要大寫 ex: X-Time, X-Sign
+		CallbackHeader: `{` +
+			`"Time": ["{{ index .header.Time 0 }}"]` +
+			`,"Sign": ["{{ index .header.Sign 0 }}"]` +
+			`}`,
+		CallbackBody: `{` +
+			`"flashid":"{{.body.flashid}}"` +
+			`,"merchant":"{{ index .query.merchant 0 }}"` +
+			`,"status":"{{ index .query.status 0 }}"` +
+			`,"payed_money":"{{ index .query.payed_money 0 }}"` +
+			`,"upstream_order":"{{ index .query.upstream_order 0 }}"` +
+			`,"repeat_pay":"{{ index .query.repeat_pay 0 }}"` +
+			`,"merchant_order_id":"{{ index .query.merchant_order_id 0 }}"` +
+			`,"message":"{{.body.message}}"` +
+			`,"order_fee":"{{ index .query.order_fee 0 }}"` +
+			`}`,
+	})
+}
